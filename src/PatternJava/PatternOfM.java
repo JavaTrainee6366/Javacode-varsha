be2173da423e8e8d0d9129755475d70768c6bd33
JavaTrainee6366 <piyugupta0786@gmail.com>
2021-12-24 10:15:48 +0530

    Adding files

---
+++ src/PatternJava/PatternOfM.java
@@ -0,0 +1,25 @@
+package PatternJava;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PatternOfM {
+
+	public static void main(String[] args) {
+		
+		int i, j, row=6;   
+		
+		for(i=0; i<row; i++)   
+		{   
+		 
+		for(j=0; j<=i; j++)   
+		{   
+		  
+		System.out.print("* ");   
+		}   
+		 
+		System.out.println();   
+		}   
+		}   
+}
+
+
